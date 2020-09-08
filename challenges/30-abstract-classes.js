283eda76587e1cf5ddeab4db66b41ff922031958
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-07 20:22:40 -0400

    Day 13: Abstract Classes complete

--- challenges/30-abstract-classes.js
+++ challenges/30-abstract-classes.js
@@ -28,23 +28,32 @@ class Book {
 }
 
 // Declare your class here.
-    /**   
-    *   Class Constructor
-    *   
-    *   @param title The book's title.
-    *   @param author The book's author.
-    *   @param price The book's price.
-    **/
-    // Write your constructor here
-    
-    /**   
-    *   Method Name: display
-    *   
-    *   Print the title, author, and price in the specified format.
-    **/
-    // Write your method here
-    
+class MyBook extends Book {
+  /**   
+  *   Class Constructor
+  *   
+  *   @param title The book's title.
+  *   @param author The book's author.
+  *   @param price The book's price.
+  **/
+  // Write your constructor here
+  constructor(title, author, price) {
+      super(title, author);
+      this.price = price;
+  }
+  /**   
+  *   Method Name: display
+  *   
+  *   Print the title, author, and price in the specified format.
+  **/
+  // Write your method here
+  display() {
+      console.log(`Title: ${this.title}`);
+      console.log(`Author: ${this.author}`);
+      console.log(`Price: ${this.price}`);
+  }
 // End class
+}
 
 function main() {
     let title = readLine()
