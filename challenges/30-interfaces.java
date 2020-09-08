851e953ab8d483505624fb2ac67bc0d82e796cd2
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-08 18:18:29 -0400

    CodeDay 19: Interfaces complete (Java)

--- challenges/30-interfaces.java
+++ challenges/30-interfaces.java
@@ -1,26 +1,32 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <string>
-using namespace std;
-class AdvancedArithmetic{
-    public:
-        virtual int divisorSum(int n)=0;
-};
-class Calculator : public AdvancedArithmetic {
-public:
-    int divisorSum(int n) {
-        return 0;
-    }
-};
+import java.io.*;
+import java.util.*;
 
-int main(){
-    int n;
-    cin >> n;
-    AdvancedArithmetic *myCalculator = new Calculator(); 
-    int sum = myCalculator->divisorSum(n);
-    cout << "I implemented: AdvancedArithmetic\n" << sum;
-    return 0;
+interface AdvancedArithmetic{
+   int divisorSum(int n);
+}
+class Calculator implements AdvancedArithmetic {
+    int sum = 0;
+    public int divisorSum(int n) {
+      for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+          sum += i;
+        }
+      }
+      return sum;
+    }
+  }
+  
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+      	AdvancedArithmetic myCalculator = new Calculator(); 
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
 }
