69a70124d147e923bb5882cb85e5da191d81a94c
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-07 22:35:11 -0400

    Day 14: Scope complete (java)

--- challenges/30-scope.java
+++ challenges/30-scope.java
@@ -6,28 +6,48 @@ import java.util.regex.*;
 
 
 class Difference {
-  	private int[] elements;
-  	public int maximumDifference;
+  private int[] elements;
+  public int maximumDifference;
 
-	// Add your code here
+// Add your code here
+  public Difference(int[] e) {
+      this.elements = e;
+  }
+
+  public void computeDifference() {
+    int a = 100;
+    int b = 1;
+    for (int i: elements) {
+        if (i <= a) {
+            a = elements[i];
+        }
+    }
+    for (int i: elements) {
+        if (i >= b) {
+            b = elements[b];
+        }
+    }
+    maximumDifference = Math.abs(b - a);
+  }
+
 
 } // End of Difference class
 
 public class Solution {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
-        }
-        sc.close();
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < n; i++) {
+          a[i] = sc.nextInt();
+      }
+      sc.close();
 
-        Difference difference = new Difference(a);
+      Difference difference = new Difference(a);
 
-        difference.computeDifference();
+      difference.computeDifference();
 
-        System.out.print(difference.maximumDifference);
-    }
+      System.out.print(difference.maximumDifference);
+  }
 }
