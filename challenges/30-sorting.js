7543d589e164df3f939698f626f8538465073189
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-08 18:29:36 -0400

    Day 20: Sorting complete
    O(n^2) time complexity

--- challenges/30-sorting.js
+++ challenges/30-sorting.js
@@ -25,5 +25,22 @@ function main() {
     a = readLine().split(' ');
     a = a.map(Number);
     // Write Your Code Here
-
+    let numberOfSwaps = 0;
+    for (let i = 0; i < a.length; i++) {
+      for (let j = 0; j < n - 1; j++) {
+        if (a[j] > a[j + 1]) {
+          let x = a[j];
+          a[j] = a[j+1];
+          a[j+1] = x;
+          numberOfSwaps++;
+        }
+      }
+  
+      if (numberOfSwaps == 0) {
+        break;
+      }
+    }
+    console.log(`Array is sorted in ${numberOfSwaps} swaps.`);
+    console.log(`First Element: ${a[0]}`);
+    console.log(`Last Element: ${a[a.length-1]}`);  
 }
