09eab862666c36546575aa6c25143f4780f6410e
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-08 18:43:21 -0400

    Day 20: Sorting finish (Java)
    Limited to using generics

--- challenges/30-generics.java
+++ challenges/30-generics.java
@@ -9,7 +9,11 @@ class Printer <T> {
     **/
     
     // Write your code here
-
+    public void printArray(T arr[]) {
+      for (int i = 0; i < arr.length; i++) {
+        System.out.println(arr[i]);
+      }
+    }
 }
 
 public class Generics {
