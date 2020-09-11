e4e08d87fda12642743829f9e5994986270e08ac
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-11 00:09:53 -0400

    Day 25: Running Time and Complexity - more efficient
    By checking if an inputted number +/- 1 is divisible by 6, we prevent entering the for-loop unless the condition is met. Furthermore, I cleaned up the code a bit.

--- challenges/30-running-time-and-complexity.js
+++ challenges/30-running-time-and-complexity.js
@@ -1,29 +1,30 @@
 function processData(input) {
   input = input.split('\n').map(number => Number(number));
-  const testCases = Number(input.shift());
-  const specialPrimes = [2,3,5];
+  input.shift();
   const types = { prime: 'Prime', notPrime: 'Not prime' };
+  const specialPrimes = [2,3];
   let message;
   for (let number of input) {
-    if (specialPrimes.includes(number)) {
-      message = types.prime;
-    } else if (number == 1) {
+    if (number == 1) {
       message = types.notPrime;
+    } else if (specialPrimes.includes(number)) {
+      message = types.prime;
     } else {
       let count = 0;
-      for (let i = 2; i <= Math.pow(number,0.5); i++) {
-        if (number % i == 0) {
-          count++;
-          break;
+      message = types.notPrime;
+      if ((number+1) % 6 == 0 || (number-1) % 6 == 0) {
+        for (let i = 2; i <= Math.pow(number,0.5); i++) {
+          if (number % i == 0) {
+            count++;
+            break;
+          }
         }
+        message = count == 0 ? types.prime : types.notPrime; 
       }
-      message = count == 0
-      ? types.prime
-      : types.notPrime; 
     }
     console.log(message);
   }
-} 
+}
 
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
