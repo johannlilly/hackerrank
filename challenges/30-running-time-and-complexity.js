a764917142b67fb6b1ff817c27dcf9a100338d0c
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-10 22:28:31 -0400

    Day 25: Running Time and Complexity - does not execute within timeline at O(N^1/2)
    10
    1000000000
    1000000001
    1000000002
    1000000003
    1000000004
    1000000005
    1000000006
    1000000007
    1000000008
    1000000009
    
    Not prime
    Not prime
    Not prime
    Not prime
    Not prime
    Not prime
    Not prime
    Prime
    Not prime
    Prime

--- challenges/30-running-time-and-complexity.js
+++ challenges/30-running-time-and-complexity.js
@@ -1,5 +1,27 @@
 function processData(input) {
-  //Enter your code here
+  input = input.split('\n').map(number => Number(number));
+  const testCases = Number(input.shift());
+  const specialPrimes = [2,3,5];
+  const types = { prime: 'Prime', notPrime: 'Not prime' };
+  let message;
+  for (let number of input) {
+      if (specialPrimes.includes(number)) {
+          message = types.prime;
+      } else if (number == 1) {
+          message = types.notPrime;
+      } else {;
+          let count = 0;
+          for (let i = 2; i < number; i++) {
+              if (number % i == 0) {
+                  count++;
+              }
+          }
+          message = count == 0
+          ? types.prime
+          : types.notPrime; 
+      }
+      console.log(message);
+  }
 } 
 
 process.stdin.resume();
