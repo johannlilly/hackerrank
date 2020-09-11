ceec345d231787838dea0c1cb69eb96fd649ec98
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-11 15:00:21 -0400

    Day 26: Nested Logic complete

--- challenges/30-nested-logic.js
+++ challenges/30-nested-logic.js
@@ -1,5 +1,32 @@
 function processData(input) {
-  //Enter your code here
+  let fine = new Number();
+  let convertToDate = arr =>
+    new Date(
+      arr.split(' ')[2],
+      arr.split(' ')[1] - 1,
+      arr.split(' ')[0]
+    );
+  
+  const returnDate = convertToDate(input.split('\n')[0]);
+  const dueDate = convertToDate(input.split('\n')[1]);
+
+  if (returnDate <= dueDate) {
+    fine = 0;
+  } else if (
+    returnDate.getFullYear() <= dueDate.getFullYear()
+    && returnDate.getMonth() <= dueDate.getMonth()
+  ) {
+    fine = 15 * (returnDate.getDate() - dueDate.getDate());
+  } else if (
+    returnDate.getFullYear() <= dueDate.getFullYear()
+    && returnDate.getMonth() > dueDate.getMonth()
+  ) {
+    fine = 500 * (returnDate.getMonth() - dueDate.getMonth());
+  } else if (returnDate.getFullYear() > dueDate.getFullYear()) {
+    fine = 10000;
+  }
+
+  console.log(fine);
 } 
 
 process.stdin.resume();
