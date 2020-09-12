4c8f8ae7659d4682527f35b49ebc6e99db35263d
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-12 17:23:38 -0400

    Day 2: Conditional Statements: Switch Exit
    start

---
+++ tutorials/10-days-of-javascript/js10-switch.js
@@ -0,0 +1,37 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    
+    return letter;
+}
+
+
+function main() {
+    const s = readLine();
+    
+    console.log(getLetter(s));
+}
