242e7a84e6787567c224bf8af339efee555c8395
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-13 18:01:22 -0400

    Day 8: Buttons Container
    cleaner solution that doesn't make some of the assumptions I previously made, such as creating a multidimentional structure.

--- tutorials/10-days-of-javascript/js10-buttons-container/js/buttonsGrid.js
+++ tutorials/10-days-of-javascript/js10-buttons-container/js/buttonsGrid.js
@@ -1,62 +1,11 @@
-// get buttons as array
-let container = document.getElementById('btns');
-let buttons = [...document.getElementsByTagName('button')];
+let values=[1,2,3,6,9,8,7,4];
+const ids=[1,2,3,6,9,8,7,4];
 
-// add (x,y) and function properties to each element
-for (btn of buttons) {
-  btn._id = Number(btn.id[3]);
-  btn.setAttribute('attr-order', buttons.indexOf(btn)+1)
-  btn.order = buttons.indexOf(btn)+1;
+let btn5=document.getElementById('btn5');
 
-  // add x
-       if (btn._id % 3 == 0) { btn.x = 3; }
-  else if (btn._id % 2 == 0 & btn._id != 4) { btn.x = 2; }
-  else if (btn._id == 5) {
-    btn.x = 2;
-    btn.onclick = () => {
-      rotate();
+btn5.onclick = () => {
+    values.unshift(values.pop());
+    for (i=0; i<=7; i++) {
+        document.getElementById('btn'+ids[i]).innerHTML=values[i];
     }
-  }
-  else { btn.x = 1; }
-
-  // add y
-       if (btn._id < 4) { btn.y = 1; }
-  else if (btn._id < 7) { btn.y = 2; }
-  else { btn.y = 3; }
-}
-
-// update (x,y) from event
-let rotate = () => {
-  for (btn of buttons) {
-         if (btn.x==1 && btn.y==1) {btn.x++;btn.order=2}
-    else if (btn.x==2 && btn.y==1) {btn.x++;btn.order=3}
-    else if (btn.x==3 && btn.y==1) {btn.y++;btn.order=6}
-
-    else if (btn.x==1 && btn.y==2) {btn.y--;btn.order=1}
-    else if (btn.x==2 && btn.y==2) {}
-    else if (btn.x==3 && btn.y==2) {btn.y++;btn.order=9}
-
-    else if (btn.x==1 && btn.y==3) {btn.y--;btn.order=4}
-    else if (btn.x==2 && btn.y==3) {btn.x--;btn.order=7}
-    else if (btn.x==3 && btn.y==3) {btn.x--;btn.order=8}
-  }
-  render();
-}
-
-
-let render = () => {
-  // update order attribute
-  for (btn of buttons) {
-    btn.setAttribute('attr-order', btn.order);
-    // btn.setAttribute('id', btn.order);
-  }
-
-  // sort by order attribute
-  buttons.sort((a,b) => a.getAttribute('attr-order')-b.getAttribute('attr-order'));
-  
-  // display
-  container.innerHTML = '';
-  for (btn of buttons) {
-    container.appendChild(btn);
-  }
 }
