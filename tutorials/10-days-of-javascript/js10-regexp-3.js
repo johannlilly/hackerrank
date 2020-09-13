501e1dcce238b8e1c05cda2a06575d721bab0d28
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-13 15:00:34 -0400

    Day 7: Regular Expressions III
    complete

--- tutorials/10-days-of-javascript/js10-regexp-3.js
+++ tutorials/10-days-of-javascript/js10-regexp-3.js
@@ -27,7 +27,7 @@ function regexVar() {
      * Declare a RegExp object variable named 're'
      * It must match ALL occurrences of numbers in a string.
      */
-    
+    let re = new RegExp('\\d+','g');
     
     /*
      * Do not remove the return statement
