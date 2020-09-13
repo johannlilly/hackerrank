c86d89a1972e9c1458a47f5132dba3ea78b22fd0
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-12 22:53:26 -0400

    Day 4: Classes
    complete. For any problem that deals with classes, it would be useful practice to implement a class using pre-ES6 syntax as well.

--- tutorials/10-days-of-javascript/js10-class.js
+++ tutorials/10-days-of-javascript/js10-class.js
@@ -3,7 +3,12 @@
  * 1. A constructor that takes an array of integer side lengths.
  * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
  */
-
+class Polygon {
+  constructor(sides) {
+      this.sides = sides;
+  }
+  perimeter() { return this.sides.reduce((acc, side) => acc + side, 0) };
+}
 
 const rectangle = new Polygon([10, 20, 10, 20]);
 const square = new Polygon([10, 10, 10, 10]);
