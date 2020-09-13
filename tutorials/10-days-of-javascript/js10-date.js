78aef1cc851fbd80ef42dc006f3fc771973b61a1
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-13 00:13:19 -0400

    Day 6: JavaScript Dates
    complete

--- tutorials/10-days-of-javascript/js10-date.js
+++ tutorials/10-days-of-javascript/js10-date.js
@@ -24,10 +24,18 @@ function readLine() {
 
 // The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
 function getDayName(dateString) {
-    let dayName;
-    // Write your code here
-    
-    return dayName;
+  let date = new Date(dateString.split('/')[2], dateString.split('/')[0]-1, dateString.split('/')[1]);
+  let day = new String();
+  switch(date.getDay()) {
+    case 0: day = "Sunday"; break;
+    case 1: day = "Monday"; break;
+    case 2: day = "Tuesday"; break;
+    case 3: day = "Wednesday"; break;
+    case 4: day = "Thursday"; break;
+    case 5: day = "Friday"; break;
+    case 6: day = "Saturday"; break;
+  }
+  return day;
 }
 
 
