785789b0715682c1b9b7d56b95a1431be8a0ef21
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-09-13 14:38:02 -0400

    Day 7: Regular Expressions II
    complete

--- tutorials/10-days-of-javascript/js10-regexp-2.js
+++ tutorials/10-days-of-javascript/js10-regexp-2.js
@@ -28,6 +28,7 @@ function regexVar() {
      * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
      * followed by one or more letters.
      */
+    let re = new RegExp('^(Mr|Mrs|Ms|Dr|Er)(\\.)([a-zA-Z])+$');
     
     /*
      * Do not remove the return statement
