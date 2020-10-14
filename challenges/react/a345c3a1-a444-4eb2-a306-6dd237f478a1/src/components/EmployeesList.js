f3195d2cac7aa3c7384b48081a9dbaff46659171
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-10-14 15:52:28 -0400

    challenges/react-filtered-employee end

--- challenges/react/a345c3a1-a444-4eb2-a306-6dd237f478a1/src/components/EmployeesList.js
+++ challenges/react/a345c3a1-a444-4eb2-a306-6dd237f478a1/src/components/EmployeesList.js
@@ -1,17 +1,29 @@
 import React from 'react';
 
 class EmployeesList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
 
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
   render() {
     const { employees } = this.props;
     return (
       <React.Fragment>
         <div className="controls">
-          <input type="text" className="filter-input" data-testid="filter-input" />
+          <input type="text" className="filter-input" data-testid="filter-input" value={this.state.value} onChange={this.handleChange} />
         </div>
         <ul className="employees-list">
           { employees.map(employee => (
-            <li key={employee.name} data-testid="employee">{employee.name}</li>
+            this.state.value === ''
+            ? <li key={employee.name} data-testid="employee">{employee.name}</li>
+            : employee.name.toUpperCase().includes(this.state.value.toUpperCase())
+              ? <li key={employee.name} data-testid="employee">{employee.name}</li>
+              : null
           ))}
         </ul>
       </React.Fragment>
