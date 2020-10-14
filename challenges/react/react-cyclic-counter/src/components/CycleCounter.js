b513df5dde4bcc8b889681388f3b2a7a55a6cffd
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-10-14 17:02:40 -0400

    challenges/react-cyclic-counter end

--- challenges/react/react-cyclic-counter/src/components/CycleCounter.js
+++ challenges/react/react-cyclic-counter/src/components/CycleCounter.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 class CycleCounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 0 };
 
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.state.value === this.props.cycle - 1
+    ? this.setState({value: 0})
+    : this.setState({value: this.state.value + 1});
+  }
   render() {
     return (
       <button
         data-testid="cycle-counter"
         style={{ fontSize: '1rem', width: 120, height: 30, }}
-      >0</button>
+        onClick={this.handleClick}
+      >{this.state.value}</button>
     );
   }
 }
