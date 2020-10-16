aa068ee522ef6d4d8e344219a17818e6ec08bd79
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-10-16 10:05:12 -0400

    challenges/react-language-translator end

--- challenges/react/react-language-translator/src/components/Translator.js
+++ challenges/react/react-language-translator/src/components/Translator.js
@@ -1,18 +1,28 @@
 import React from 'react';
 
 class Translator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { word: '' };
 
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.props.translations.has(event.target.value)
+    ? this.setState({word: this.props.translations.get(event.target.value)})
+    : this.setState({word: ''})
+  }
   render() {
     return (
       <React.Fragment>
         <div className="controls">
           <div className="input-container">
             <span>input:</span>
-            <input type="text" className="text-input" data-testid="text-input" />
+            <input type="text" className="text-input" data-testid="text-input" onChange={this.handleChange} />
           </div>
           <div className="input-container">
             <span>output:</span>
-            <input type="text" className="text-output" data-testid="text-output" readOnly />
+            <input type="text" className="text-output" data-testid="text-output" value={this.state.word} readOnly />
           </div>
         </div>
       </React.Fragment>
