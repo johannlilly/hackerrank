1e6fad6f83587b211d1320ff60057756eeb6c84a
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-10-16 21:49:39 -0400

    challenges/react-paginated-articles end
    - upgraded nvm
    - useState

--- challenges/react/react-paginated-articles/src/components/Articles.js
+++ challenges/react/react-paginated-articles/src/components/Articles.js
@@ -1,22 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-class Articles extends React.Component {
+const API_URL = 'https://jsonmock.hackerrank.com/api/articles';
 
-  render() {
-    return (
-      <React.Fragment>
-        <div className="pagination">
-          <button data-testid="page-button" key="page-button-1">1</button>
-          <button data-testid="page-button" key="page-button-2">2</button>
-        </div>
-        <ul className="results">
-          <li key="title-1" data-testid="result-row">Example title 1</li>
-          <li key="title-2" data-testid="result-row">Example title 2</li>
-          <li key="title-3" data-testid="result-row">Example title 3</li>
-        </ul>
-      </React.Fragment>
-    );
-  }
-}
+const Articles = () => {
+  const [articles, setArticles] = useState([]);
+  const [pages, setPages] = useState(null);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+
+  const retrieveArticles = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch(`${API_URL}?page=${page}`);
+      const { total_pages, data } = await res.json();
+      setPages(total_pages);
+      setArticles(data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleClick = (p) => {
+    if (loading) return;
+
+    setPage(p);
+  };
+
+  useEffect(() => {
+    retrieveArticles();
+  }, [page]);
+
+  return (
+    <React.Fragment>
+      <div className="pagination">
+        {pages &&
+          Array(pages)
+            .fill()
+            .map((_, i) => (
+              <button
+                disabled={loading}
+                onClick={() => handleClick(i + 1)}
+                data-testid="page-button"
+                key={`page-button-${i + 1}`}
+              >
+                {i + 1}
+              </button>
+            ))}
+      </div>
+      <ul className="results">
+        {articles.filter(({ title }) => title).map(({ title }) => (
+          <li key={title} data-testid="result-row">
+            {title}
+          </li>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
+};
 
 export default Articles;
