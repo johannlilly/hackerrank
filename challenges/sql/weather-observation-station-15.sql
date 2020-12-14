4c0f010b7d1add3f70868cbb72861510d42f5cae
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-12-14 15:53:34 -0500

    challenges/weather-observation-station-15

---
+++ challenges/sql/weather-observation-station-15.sql
@@ -0,0 +1,9 @@
+-- Practice > SQL > Aggregation > Weather Observation Station 15
+-- Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 1372345.
+-- Round your answer to 4 decimal places.
+
+SELECT ROUND(LONG_W, 4)
+FROM STATION
+WHERE LAT_N < 137.2345
+ORDER BY LAT_N desc
+LIMIT 1;
