574b35b3ad4d547a7ee59351afd79329210e547d
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-12-14 15:34:16 -0500

    challenges/weather-observation-station-13

---
+++ challenges/sql/weather-observation-station-13.sql
@@ -0,0 +1,8 @@
+-- Practice > SQL > Aggregation > Weather Observation Station 13
+-- Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345.
+-- Truncate your answer to 4 decimal places.
+
+SELECT SUM(LAT_N)
+FROM STATION
+WHERE LAT_N > 38.7880
+  AND LAT_N < 137.2345;
