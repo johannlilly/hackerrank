fa055ce5b49325ffebf2af4f245984c69e63e3da
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-11 11:51:17 -0500

    challenges/java-if-else
    start

---
+++ challenges/java/java-if-else.java
@@ -0,0 +1,32 @@
+/**
+ * Practice > Java > Java If-Else
+ * 
+ * Given an integer, n, perform the following conditional actions:
+ *  If n is odd, print Weird
+ *  If n is even and in the inclusive range of 2 to 5, print Not Weird
+ *  If n is even and in the inclusive range of 6 to 20, print Weird
+ *  If n is even and greater than 20, print Not Weird
+ * Complete the stub code provided in your editor to print whether or not n is weird.
+ * 
+ * Version: Java 8
+ */
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+    }
+}
