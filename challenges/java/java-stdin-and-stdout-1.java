970cf2573ab22221149b26dffad047f9c786085a
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-11 11:44:02 -0500

    challenges/java-stdin-and-stdout-1
    Finish. Not very programmatic, but fulfills the task requirements. To scale, change scanning to a do-while loop or look into accumulators.

--- challenges/java/java-stdin-and-stdout-1.java
+++ challenges/java/java-stdin-and-stdout-1.java
@@ -1,15 +1,26 @@
+/**
+ * Practice > Java > Introduction Java Stdin and Stdout I
+ * 
+ * In this challenge, you must read 3 integers from stdin and then print them to stdout.
+ * Each integer must be printed on a new line.
+ * To make the problem a little easier, a portion of the code is provided for you in the editor below.
+ * 
+ * Version: Java 8
+ */
+
 import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        // Complete this line
-        // Complete this line
+      Scanner scan = new Scanner(System.in);
+      int myInt1 = scan.nextInt();
+      int myInt2 = scan.nextInt();
+      int myInt3 = scan.nextInt();
+      scan.close();        
 
-        System.out.println(a);
-        // Complete this line
-        // Complete this line
+      System.out.println(myInt1);
+      System.out.println(myInt2);
+      System.out.println(myInt3);
     }
 }
