8f967e09f4ee513c70db97e1ac217b7c2d986876
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-11 12:03:37 -0500

    challenges/java-if-else
    Solved using nested IF statements

--- challenges/java/java-if-else.java
+++ challenges/java/java-if-else.java
@@ -21,12 +21,25 @@ import java.util.regex.*;
 
 public class Solution {
 
-    private static final Scanner scanner = new Scanner(System.in);
+  private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        int N = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+  public static void main(String[] args) {
+    int n = scanner.nextInt();
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+    scanner.close();
 
-        scanner.close();
+    if (n % 2 == 0) {
+      if (2 <= n && n <= 5) {
+        System.out.println("Not Weird");
+      }
+      if (6 <= n && n <= 20) {
+        System.out.println("Weird");
+      }
+      if (20 < n) {
+        System.out.println("Not Weird");
+      }
+    } else if (n % 2 != 0) {
+      System.out.println("Weird");
     }
+  }
 }
