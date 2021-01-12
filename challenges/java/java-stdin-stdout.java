e7764446e8d5383c98d778a2b16a8e5890146aae
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-12 15:18:58 -0500

    challenges/java-stdin-stdout
    complete

--- challenges/java/java-stdin-stdout.java
+++ challenges/java/java-stdin-stdout.java
@@ -5,6 +5,10 @@
  * then print the values according to the instructions in the Output Format section below.
  * To make the problem a little easier, a portion of the code is provided for you in the editor.
  * 
+ * On the first line, print String: followed by the unaltered String read from stdin.
+ * On the second line, print Double: followed by the unaltered double read from stdin.
+ * On the third line, print Int: followed by the unaltered integer read from stdin.
+ * 
  * Version: Java 8
  */
 
@@ -14,9 +18,11 @@ public class Solution {
 
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    int i = scan.nextInt();
 
-    // Write your code here.
+    int i = scan.nextInt();
+    double d = scan.nextDouble();
+               scan.nextLine();
+    String s = scan.nextLine();
 
     System.out.println("String: " + s);
     System.out.println("Double: " + d);
