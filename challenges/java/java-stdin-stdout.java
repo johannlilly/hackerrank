140f781f816ea4ae712cceadbb9bf676d57494e2
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-12 14:20:07 -0500

    challenges/java-stdin-stdout
    start

---
+++ challenges/java/java-stdin-stdout.java
@@ -0,0 +1,25 @@
+/**
+ * Practice > Java > Introduction Java Stdin and Stdout II
+ * 
+ * In this challenge, you must read an integer, a double, and a String from stdin,
+ * then print the values according to the instructions in the Output Format section below.
+ * To make the problem a little easier, a portion of the code is provided for you in the editor.
+ * 
+ * Version: Java 8
+ */
+
+import java.util.Scanner;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int i = scan.nextInt();
+
+    // Write your code here.
+
+    System.out.println("String: " + s);
+    System.out.println("Double: " + d);
+    System.out.println("Int: " + i);
+  }
+}
