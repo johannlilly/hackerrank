bbb20843f4772c8c2878b1e8e57e50dc501c01c0
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-13 21:29:22 -0500

    challenges/java-output-formatting
    start

---
+++ challenges/java/java-output-formatting.java
@@ -0,0 +1,29 @@
+/**
+ * Practice > Java > Introduction > Java Output Formatting
+ * 
+ * In each line of output there should be two columns:
+ * The first column contains the String and is left justified using exactly 15 characters.
+ * The second column contains the integer, expressed in exactly 3 digits;
+ * if the original input has less than three digits, you must pad your output's leading digits with zeroes.
+
+ * 
+ * Version: Java 8
+ */
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+            }
+            System.out.println("================================");
+
+    }
+}
