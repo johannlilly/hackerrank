4a5caa82b1f4cf6f484f2f2800caae4d2dc99335
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-13 22:02:08 -0500

    challenges/java-output-formatting
    solved using ternary operator, but its ugly

--- challenges/java/java-output-formatting.java
+++ challenges/java/java-output-formatting.java
@@ -13,17 +13,27 @@
 import java.util.Scanner;
 
 public class Solution {
-
-    public static void main(String[] args) {
-            Scanner sc=new Scanner(System.in);
-            System.out.println("================================");
-            for(int i=0;i<3;i++)
-            {
-                String s1=sc.next();
-                int x=sc.nextInt();
-                //Complete this line
-            }
-            System.out.println("================================");
-
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("================================");
+    for(int i=0;i<3;i++)
+    {
+      String s1=sc.next();
+      int x=sc.nextInt();
+      System.out.println(
+        s1
+          + (' ' * (15 - s1.length))
+          + (
+          x < 100
+          ? "0" + x
+          : x < 10
+            ? "00" + x
+            : x
+        )
+      );
     }
+    System.out.println("================================");
+  }
 }
+
+java           11
