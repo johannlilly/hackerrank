b42d9901937f1d20571aa4a1a97b33fe2794ad71
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-16 11:22:48 -0500

    challenges/java-loops-i
    solved

--- challenges/java/java-loops-i.java
+++ challenges/java/java-loops-i.java
@@ -23,6 +23,10 @@ public class Solution {
     int N = scanner.nextInt();
     scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
+    for (int i = 1; i < 11; i++) {
+      System.out.println(N + " x " + i + " = " + N * i);
+    }
+
     scanner.close();
   }
 }
