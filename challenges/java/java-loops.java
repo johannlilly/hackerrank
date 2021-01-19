68b6c092bd3b5a8fbe6e056fd5e63fd1ed07005c
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2021-01-18 23:25:32 -0500

    challenges/java-loops
    start

---
+++ challenges/java/java-loops.java
@@ -0,0 +1,24 @@
+/**
+ * Practice > Java > Introduction > Java Loops II
+ * 
+ * You are given q queries in the form of a, b, and n.
+ * For each query, print the series corresponding to the given a, n, and n values as a single line of n space-separated integers.
+ * 
+ * Version: Java 8
+ */
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int t=in.nextInt();
+    for(int i=0;i<t;i++){
+      int a = in.nextInt();
+      int b = in.nextInt();
+      int n = in.nextInt();
+    }
+    in.close();
+  }
+}
