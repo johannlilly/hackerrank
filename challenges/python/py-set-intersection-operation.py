4afa1d44c5452fe9e25023fed6d80c5fbe6ef46f
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2024-07-23 22:26:54 -0400

    Set .intersection() Operation (Python)

---
+++ challenges/python/py-set-intersection-operation.py
@@ -0,0 +1,16 @@
+# Set .intersection() Operation
+# https://www.hackerrank.com/challenges/py-set-intersection-operation/problem
+
+if __name__ == '__main__':
+  english = int(input())
+  en_subs = set(input().split(" "))
+  french = int(input())
+  fr_subs = set(input().split(" "))
+
+# test
+# english = 9
+# en_subs = set("1 2 3 4 5 6 7 8 9".split(" "))
+# french = 9
+# fr_subs = set("10 1 2 3 11 21 55 6 8".split(" "))
+
+print(len(en_subs.intersection(fr_subs)))
