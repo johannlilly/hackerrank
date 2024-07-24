967e7142410fcaeb5145858aa06d529ea63e7550
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2024-07-24 17:18:38 -0400

    sWAP cASE

---
+++ challenges/python/swap-case.py
@@ -0,0 +1,10 @@
+# Python > Strings > sWAP cASE
+# hackerrank.com/challenges/swap-case
+
+def swap_case(s):
+  return s.swapcase()
+
+if __name__ == '__main__':
+  s = input()
+  result = swap_case(s)
+  print(result)
