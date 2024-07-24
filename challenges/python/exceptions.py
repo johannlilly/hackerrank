6e8ef71c75792abc14747ab689dc3d9d81bcafd3
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2024-07-24 13:56:44 -0400

    Exceptions (Python)

---
+++ challenges/python/exceptions.py
@@ -0,0 +1,9 @@
+# Exceptions
+# https://www.hackerrank.com/challenges/exceptions/problem
+
+for i in range(int(input())):
+  try:
+    a, b = map(int, input().split())
+    print(a//b)
+  except BaseException as e:
+    print("Error Code:", e)
