154a77935416b70e2afd72fceffe6325e8fd7b6b
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2024-07-24 16:44:44 -0400

    Tuples (Python)
    Code did not work in Python 3, but did work in Pypy 3.

---
+++ challenges/python/python-tuples.py
@@ -0,0 +1,8 @@
+# Tuples
+# https://www.hackerrank.com/challenges/python-tuples/problem
+
+if __name__ == '__main__':
+  n = int(input())
+  integer_list = list(map(int, input().split(" ")))
+  t = tuple(integer_list)
+  print(hash(t))
