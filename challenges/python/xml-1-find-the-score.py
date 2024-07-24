30a3270a0024ec9add6eb4889433e27a77f94b1a
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2024-07-24 14:32:42 -0400

    XML 1 - Find the Score (Python)

---
+++ challenges/python/xml-1-find-the-score.py
@@ -0,0 +1,16 @@
+# XML 1 - Find the Score
+# https://www.hackerrank.com/challenges/xml-1-find-the-score/problem
+
+import sys
+import xml.etree.ElementTree as etree
+
+def get_attr_number(node):
+  # your code goes here
+  return sum([len(elem.items()) for elem in tree.iter()])
+
+if __name__ == '__main__':
+  sys.stdin.readline()
+  xml = sys.stdin.read()
+  tree = etree.ElementTree(etree.fromstring(xml))
+  root = tree.getroot()
+  print(get_attr_number(root))
