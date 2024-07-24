f57fbfc5a1de0169d787ee390b4e3d9cfd5658c5
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2024-07-24 15:38:59 -0400

    Sum and Prod (Python)

---
+++ challenges/python/np-sum-and-prod.py
@@ -0,0 +1,10 @@
+# Sum and Prod
+# https://www.hackerrank.com/challenges/np-sum-and-prod/problem
+
+import numpy
+
+N, M = map(int, input().split())
+
+A = numpy.array([input().split() for _ in range(N)], int)
+
+print(numpy.prod(numpy.sum(A, axis=0), axis=0))
