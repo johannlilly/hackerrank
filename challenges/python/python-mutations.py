10d6c268bd61a7ca3c329648e69c66329a8db535
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2024-07-24 17:44:49 -0400

    Mutations (Python)

---
+++ challenges/python/python-mutations.py
@@ -0,0 +1,12 @@
+# Prepare > Python > Strings > Mutations
+# hackerrank.com/challenges/python-mutations
+
+def mutate_string(string, position, character):
+  string = string[:position] + character + string[position + 1:]
+  return string
+
+if __name__ == '__main__':
+  s = input()
+  i, c = input().split()
+  s_new = mutate_string(s, int(i), c)
+  print(s_new)
