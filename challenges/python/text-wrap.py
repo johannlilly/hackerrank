75a0af94880fcc72495ec5e98cd2d49c96348540
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2024-07-25 23:03:47 -0400

    Text Wrap (Python) Start

---
+++ challenges/python/text-wrap.py
@@ -0,0 +1,12 @@
+# Prepare > Python > Strings > Text Wrap
+# hackerrank.com/challenges/text-wrap
+
+import textwrap
+
+def wrap(string, max_width):
+    return
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
