76adb13a521efa75f0463adada2b0b9600d6dd68
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2024-07-26 15:34:03 -0400

    Designer Door Mat (Python)

---
+++ challenges/python/designer-door-mat.py
@@ -0,0 +1,6 @@
+# Prepare > Python > Strings > Designer Door Mat
+# hackerrank.com/challenges/designer-door-mat
+
+n, m = map(int, input().split())
+pattern = [('.|.' * (2 * i + 1)).center(m, '-') for i in range(n // 2)]
+print('\n'.join(pattern + ['WELCOME'.center(m, '-')] + pattern[::-1]))
