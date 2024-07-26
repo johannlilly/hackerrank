71a87aee2187661628278a4b7862096927f0238d
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2024-07-26 15:28:55 -0400

    Text Wrap (Python)
    Solved

--- challenges/python/text-wrap.py
+++ challenges/python/text-wrap.py
@@ -4,7 +4,18 @@
 import textwrap
 
 def wrap(string, max_width):
-    return
+    wrapped = ""
+
+    for i in range(len(string)):
+
+        if i > 1 and i % max_width == 0:
+        
+            print(wrapped)
+            wrapped = ""
+
+        wrapped += string[i]
+
+    return wrapped
 
 if __name__ == '__main__':
     string, max_width = input(), int(input())
