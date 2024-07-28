175e8576a590880c215cd318aafb466b10b143c1
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2024-07-28 12:52:29 -0400

    PreparePythonItertoolsitertools.combinations_with_replacement() (Python)

--- challenges/python/itertools-combinations-with-replacement.py
+++ challenges/python/itertools-combinations-with-replacement.py
@@ -1,3 +1,14 @@
 # Prepare > Python > Itertools > itertools.combinations_with_replacement()
 # hackerrank.com/challenges/itertools-combinations-with-replacement
 
+from itertools import combinations_with_replacement
+
+given = input().split()
+
+S = list(given[0])
+K = int(given[1])
+
+S.sort()
+
+for combo in combinations_with_replacement(S, K):
+    print("".join(combo))
