b256bc71a9d59d47324d8d27303e08525087f4df
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2024-08-04 17:31:09 -0400

    Sparse Arrays (Python)
    O(n^2)

---
+++ challenges/python/sparse-arrays.py
@@ -0,0 +1,61 @@
+# HackerRank > Prepare > Data Structures > Arrays > Sparse Arrays
+# hackerrank.com/challenges/sparse-arrays
+
+# How many times does it occur in the list of input strings?
+# results = [frequency of items in stringList]
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY stringList
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(stringList, queries):
+    queryCounter = []
+
+    for query in queries:
+        counter = 0
+        for string in stringList:
+            if query == string:
+                counter += 1
+
+        queryCounter.append(counter)
+
+    return queryCounter
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    stringList_count = int(input().strip())
+
+    stringList = []
+
+    for _ in range(stringList_count):
+        stringList_item = input()
+        stringList.append(stringList_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(stringList, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
