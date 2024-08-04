2dcd155abff2608cde6ef5f1713abdc21415157c
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2024-08-04 16:25:39 -0400

    2D Array - DS (Python)
    X and Y arrangement is a little confusing.

---
+++ challenges/python/2d-array.py
@@ -0,0 +1,52 @@
+# HackerRank > Prepare > Data Structures > Arrays > 2D Array - DS
+# hackerrank.com/challenges/2d-array
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    # Write your code here
+
+    sums = []
+
+    for y in range(len(arr) - 2):
+        for x in range(len(arr[y]) - 2):
+            a = arr[y][x]
+            b = arr[y][x+1]
+            c = arr[y][x+2]
+            d = arr[y+1][x+1]
+            e = arr[y+2][x]
+            f = arr[y+2][x+1]
+            g = arr[y+2][x+2]
+            
+            sums.append(a+b+c+d+e+f+g)
+
+    sums.sort()
+    
+    return(sums[-1])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
